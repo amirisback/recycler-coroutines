4c3f9eea27d01432704a5f9d1028baad5c32f4d6
Muhammad Faisal Amir <faisalamircs@gmail.com>
2022-02-07 13:53:02 +0700

    created easy recyclerview

--- app/src/main/java/com/frogobox/recyclercoroutines/MainHolder.kt
+++ app/src/main/java/com/frogobox/recyclercoroutines/MainHolder.kt
@@ -26,7 +26,7 @@ class MainHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val imageProfileGithub = "https://avatars.githubusercontent.com/u/24654871?s=400&u=a988212f6f21a08c8b56775afd1d85c5104b3c8c&v=4"
 
         view.findViewById<TextView>(R.id.frogo_rv_list_type_11_tv_title).text = data.name
-        view.findViewById<TextView>(R.id.frogo_rv_list_type_11_tv_desc).text = data.name
+        view.findViewById<TextView>(R.id.frogo_rv_list_type_11_tv_desc).text = data.age.toString()
         view.findViewById<TextView>(R.id.frogo_rv_list_type_11_tv_subtitle).text = data.name
         Glide.with(view.context).load(imageProfileGithub).into(view.findViewById(R.id.frogo_rv_list_type_11_iv_poster))
         itemView.setOnClickListener { listener.onClickListener(data) }
