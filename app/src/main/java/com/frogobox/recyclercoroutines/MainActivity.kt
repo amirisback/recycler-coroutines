b4ea81e1b1734f0094aa3a2cf768f1683bf739f9
Muhammad Faisal Amir <faisalamircs@gmail.com>
2022-02-07 13:52:05 +0700

    created easy recyclerview

--- app/src/main/java/com/frogobox/recyclercoroutines/MainActivity.kt
+++ app/src/main/java/com/frogobox/recyclercoroutines/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.frogobox.recyclercoroutines
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.frogobox.recyclercoroutines.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainItemListener {
+
+    private val data = mutableListOf<MainData>()
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setupRV()
     }
+
+    fun setupData(): MutableList<MainData> {
+        data.add(MainData("Faisal Amir", 24))
+        return data
+    }
+
+    fun setupRV() {
+        val mainAdapter = MainAdapter(this)
+        mainAdapter.setContent(setupData())
+
+        binding.rvFrogo.apply {
+            adapter = mainAdapter
+            layoutManager =
+                LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+        }
+
+    }
+
+    override fun onClickListener(data: MainData) {
+    }
+
+
 }
